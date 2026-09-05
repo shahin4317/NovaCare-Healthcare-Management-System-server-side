e2f3160817dafd27365284edd0516b27e7cbe77b
shahin4317 <shahinalam4317@gmail.com>
2026-09-05 22:45:27 +0600

    error fixed

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // const database = client.db('novacare-db')
     // const doctorCollection =database.collection('doctor')
